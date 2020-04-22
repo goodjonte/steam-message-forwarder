729c4d2b2cd3e02589d6b136d13e4e051a1779d0
goodjonte <goodjonte@gmail.com>
2020-04-22 23:04:57 +1200

    Commented for clarity

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 module.exports = {
 
     bot: {
-        username: '' ,                //account username
-        password: '' ,                //account password
-        shared_secret: '' ,           //shared secret (from SDA) tutorial: https://youtu.be/JjdOJVSZ9Mo
-        main_acc_ID: '',              //steamID of master account from https://steamidfinder.com/  E.G. STEAM_1:1:89842841
-        main_acc_ID64: ''             //steamID64 of master account from https://steamidfinder.com/  E.G. 76561198139951411
+        
+        username: '' ,                //account username of ALT ACCOUNT
+        password: '' ,                //account password of ALT ACCOUNT
+        shared_secret: '' ,           //shared secret of ALT ACCOUNT (from SDA) tutorial: https://youtu.be/JjdOJVSZ9Mo
+        
+        
+        main_acc_ID: '',              //steamID of MASTER/MAIN account from https://steamidfinder.com/  E.G. STEAM_1:1:89842841
+        main_acc_ID64: ''             //steamID64 of MASTER/MAIN  account from https://steamidfinder.com/  E.G. 76561198139951411
     },
 
 
